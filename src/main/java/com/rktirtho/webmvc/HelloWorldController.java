dca90bfee2294b6e4266d0db0510df8efc817c53
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-02-07 18:36:54 +0600

    request param example added

--- src/main/java/com/rktirtho/webmvc/HelloWorldController.java
+++ src/main/java/com/rktirtho/webmvc/HelloWorldController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -24,8 +25,18 @@ public class HelloWorldController {
 	}
 	
 	@RequestMapping("process-form")
-	public String processForm(HttpServletRequest request, Model model) {
+	public String processFormTwo(HttpServletRequest request, Model model) {
 		String name = request.getParameter("studentName");
+		
+		model.addAttribute("name",name);
+		
+		
+		return "process-hello-form";
+	}
+	
+	@RequestMapping("process-form")
+	public String processFormThree(@RequestParam("studentName") String name
+			, Model model) {
 		
 		model.addAttribute("name",name);
 		
